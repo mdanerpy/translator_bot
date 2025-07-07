e54f4689ac0d9881e2f456debe4a3f35fbb01b00
mdanerpy <eliaelia03130313@gmail.com>
2025-07-07 11:24:53 +0330

    Update translator_bot.py

--- translator_bot.py
+++ translator_bot.py
@@ -2,9 +2,10 @@ from telegram import Update
 from telegram.ext import ApplicationBuilder, MessageHandler, ContextTypes, filters
 from deep_translator import GoogleTranslator
 import os
+import threading
+from http.server import BaseHTTPRequestHandler, HTTPServer
 
 def detect_language(text):
-    # بررسی وجود حروف فارسی
     for ch in text:
         if '\u0600' <= ch <= '\u06FF':
             return 'fa'
@@ -21,10 +22,19 @@ async def translate_message(update: Update, context: ContextTypes.DEFAULT_TYPE):
 
     await update.message.reply_text(translated)
 
+def fake_server():
+    port = int(os.environ.get("PORT", 8080))
+    server = HTTPServer(("", port), BaseHTTPRequestHandler)
+    server.serve_forever()
+
 if __name__ == '__main__':
-    TOKEN = os.environ["BOT_TOKEN"]  # توکن رو از تنظیمات Render می‌گیره
+    # اجرای پورت الکی برای فریب Render
+    threading.Thread(target=fake_server).start()
+
+    # اجرای ربات
+    TOKEN = os.environ["BOT_TOKEN"]
     app = ApplicationBuilder().token(TOKEN).build()
     app.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, translate_message))
 
-    print("ربات مترجم آماده اجراست...")
+    print("✅ ربات مترجم آماده اجراست...")
     app.run_polling()
